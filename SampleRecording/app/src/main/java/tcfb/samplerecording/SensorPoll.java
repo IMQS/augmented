30bd28126cc61ab8d1e624fcf066fc8ec84dae5e
M. H. Esterhuizen <marvin.esterhuizen@gmail.com>
2016-01-07 13:46:23 +0200

    SensorPoll file output

--- SampleRecording/app/src/main/java/tcfb/samplerecording/SensorPoll.java
+++ SampleRecording/app/src/main/java/tcfb/samplerecording/SensorPoll.java
@@ -3,7 +3,14 @@ package tcfb.samplerecording;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
+import java.util.Map;
 
 /**
  * Created by marvin on 2016/01/06.
@@ -14,19 +21,51 @@ import android.hardware.SensorManager;
  * measurement for each sensor can be attached to each camera frame.
  */
 public class SensorPoll implements SensorEventListener {
-    SensorEvent accelEvent = null, gyroEvent = null, magneticFieldEvent = null;
+    private static final String SENSOR_DATA_DIR= "sensor_data";
+    private long startTime, elapsedTime;
+    private SensorEvent accelEvent = null, gyroEvent = null, magneticFieldEvent = null;
+    private File appDir, outFile;
+    private BufferedWriter bw = null;
+    private boolean start = false;
+
+    public SensorPoll(File appDir, long time) {
+        this.startTime = startTime;
+        this.appDir = appDir;
+
+        File dataDir = new File(appDir, SENSOR_DATA_DIR);
+        if (!dataDir.exists()) {
+            dataDir.mkdir();
+        }
+
+        int numFiles = dataDir.listFiles().length;
+        outFile = new File(dataDir, "sensor_data_" + numFiles + ".sd");
+        try {
+            bw = new BufferedWriter(new FileWriter(outFile));
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+
+    public void start() {
+        start = true;
+    }
+
+    public void stop() {
+        start = false;
+    }
 
     /* Returns last recorded acceleration measurement. Returns null if no measurement has been
      * made yet.
      */
     public SensorEvent pollAccel() {
-       return accelEvent;
+        elapsedTime = System.currentTimeMillis() - startTime;
+        return accelEvent;
     }
-
     /* Returns last recorded gyroscope measurement. Returns null if no measurement has been
      * made yet.
      */
     public SensorEvent pollGyro() {
+        elapsedTime = System.currentTimeMillis() - startTime;
         return gyroEvent;
     }
 
@@ -34,19 +73,40 @@ public class SensorPoll implements SensorEventListener {
      * made yet.
      */
     public SensorEvent pollMagneticField() {
+        elapsedTime = System.currentTimeMillis() - startTime;
         return magneticFieldEvent;
     }
 
-    /* Do nothing because I don't even care right now */
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public Map<String, Object> pollAll() {
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        map.put("accelerometer", pollAccel());
+        map.put("gyroscope", pollGyro());
+        map.put("magnetic_field", pollMagneticField());
+        map.put("time", elapsedTime);
+        return map;
     }
+
+    public void setTime(long time) { this.startTime = time; }
+
+    /* Do nothing because I don't even care right now */
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 
     /* Update sensor data */
     public void onSensorChanged(SensorEvent event) {
         switch (event.sensor.getType()) {
             case Sensor.TYPE_ACCELEROMETER: accelEvent = event; break;
-            case Sensor.TYPE_GYROSCOPE: gyroEvent = event; break;
             case Sensor.TYPE_MAGNETIC_FIELD: magneticFieldEvent = event; break;
+            case Sensor.TYPE_GYROSCOPE: gyroEvent = event; break;
+        }
+
+        if (!start) return;
+        try {
+            bw.write(accelEvent.values[0] + " " + accelEvent.values[1] + " " + accelEvent.values[2]);
+            bw.write(gyroEvent.values[0] + " " + gyroEvent.values[1] + " " + gyroEvent.values[2]);
+            bw.write(magneticFieldEvent.values[0] + " " + magneticFieldEvent.values[1] + " " + magneticFieldEvent.values[2]);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
     }
 
