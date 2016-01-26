c16d8cbd1dd43e1615d8964b18f640aad50f3082
arjen.singels <arjen.singels@gmail.com>
2016-01-26 10:24:25 +0200

    Fixed Euler angels to quaternion conversion

--- SensorPipes/app/src/main/java/com/example/erik/SensorPipes/utilities/EulerToQuat.java
+++ SensorPipes/app/src/main/java/com/example/erik/SensorPipes/utilities/EulerToQuat.java
@@ -6,27 +6,31 @@ package com.example.erik.SensorPipes.utilities;
 public class EulerToQuat {
 
     //Look away now!
-    public float[] convertEulerAnglesToQuaternion(float xRotation,float yRotation,float zRotation){
-        //xRot = bank (roll)
+    public static double[] convertEulerAnglesToQuaternion(double xRotation,double yRotation,double zRotation){
+        //Converted using these standards http://www.euclideanspace.com/maths/standards/index.htm
+
         //yRot = attitude (pitch)
         //zRot = heading (yaw)
+        //xRot = bank (roll)
+        double yRot = yRotation * Math.PI/360;
+        double zRot = zRotation * Math.PI/360;
+        double xRot = xRotation * Math.PI/360;
 
         //Your last chance to look away!
-        float [] quaternion = new float[4];
-        quaternion[1] = cos(zRotation / 2) * cos(yRotation / 2) * cos(xRotation/2) - sin(zRotation / 2) * sin(yRotation / 2) * sin(xRotation/2);
-        quaternion[2] = sin(zRotation / 2) * sin(yRotation / 2) * cos(xRotation/2) - cos(zRotation / 2) * cos(yRotation / 2) * sin(xRotation/2);
-        quaternion[3] = sin(zRotation/2) * cos(yRotation/2) * cos(xRotation/2) - cos(zRotation/2) * sin(yRotation/2) * sin(xRotation/2);
-        quaternion[4] = cos(zRotation/2) * sin(yRotation/2) * cos(xRotation/2) - sin(zRotation/2) * cos(yRotation/2) * sin(xRotation/2);
+        double [] quaternion = new double[4];
+        quaternion[0] = cos(zRot) * cos(yRot) * cos(xRot) - sin(zRot) * sin(yRot) * sin(xRot);
+        quaternion[1] = sin(zRot) * sin(yRot) * cos(xRot) + cos(zRot) * cos(yRot) * sin(xRot);
+        quaternion[2] = sin(zRot) * cos(yRot) * cos(xRot) + cos(zRot) * sin(yRot) * sin(xRot);
+        quaternion[3] = cos(zRot) * sin(yRot) * cos(xRot) - sin(zRot) * cos(yRot) * sin(xRot);
         return quaternion;
     }
 
     //you got this far, nice! but srs. Stop looking.
-    private float cos(float in){
-        return (float)Math.cos(in);
+    private static double cos(double in){
+        return (double)Math.cos(in);
     }
-    private float sin(float in){
-        return (float)Math.sin(in);
+    private static double sin(double in){
+        return (double)Math.sin(in);
     }
     //jk. not that bad was it.
-
 }
